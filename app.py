ae8d5d2475f55a1d2b3949aa8a8f095263dd0dad
shibu8-dotcom <shivanikanojiya217@gmail.com>
2026-01-02 13:01:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 from flask_cors import CORS
 import sqlite3
 from datetime import datetime
@@ -112,4 +113,5 @@ if __name__ == "__main__":
 
     app.run(host="0.0.0.0",
     port=int(os.environ.get("PORT",5000)))
+
 
